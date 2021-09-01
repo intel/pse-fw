4754143ee79750c71d4a66575fd2c2aefbd7ac75
Nachiketa Kumar <nachiketa.kumar@intel.com>
2021-09-01 21:12:51 +0530

    build : add support for bootloader build
    Updates west.yml to fetch bootloader sources and dependencies
    
    Enables building of AON and BUP binaries from source.
    
    Signed-off-by: Nachiketa Kumar <nachiketa.kumar@intel.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 ws_dir=$(pwd)
 SERVICES=$(pwd)/services
 pse_image_tools_dir=$ws_dir/tools/pse_image_tool
+bootloader_dir=$ws_dir/../modules/bootloader
 
 clean() {
 	if [ -d "build" ]; then
@@ -13,18 +14,32 @@ clean() {
 	if [ -d $pse_image_tools_dir/output ]; then
 		rm -rf $pse_image_tools_dir/output
 	fi
+	pushd $bootloader_dir > /dev/null
+	cd aon_task;
+	if [ -f aon_task.bin ]; then
+		make clean;
+	fi
+	cd ../bringup
+	if [ -f bringup.bin ]; then
+		make clean;
+	fi
+
+	popd > /dev/null
+
 }
 
 if [ "$1" == "clean" ]; then
-	echo "Cleaning"
+	echo "Cleaning.."
 	clean
+	echo "done"
 	exit 0
 fi
 
+
 if [ "$1" == "" ]
 then
-	app=$ws_dir/../zephyr/samples/hello_world
-	echo "Building Hello World"
+	app=$ws_dir/apps/capabilities/pse_base
+	echo "Building pse_base app"
 else
 	app=$1
 fi
@@ -41,13 +56,23 @@ if [ $(test) -n "$(find build -name zephyr.elf)" ]; then
 	ZEPHYR_MAP=$(find build -name zephyr.map)
 	mkdir $pse_image_tools_dir/fragments
 	cp $ZEPHYR_ELF $ZEPHYR_MAP $pse_image_tools_dir/fragments
-	if [ -f "tools/bin/aon_task.bin" ]; then
-		echo "found aon_task bin"
-		cp tools/bin/aon_task.bin $pse_image_tools_dir/fragments
-	fi
-	if [ -f "tools/bin/bringup.bin" ]; then
-		echo "found bringup bin"
-		cp tools/bin/bringup.bin $pse_image_tools_dir/fragments
+	if [ "$2" == "CUSTOMER" ]; then
+		echo "CUSTOMER BUILD"
+		if [ -f "tools/bin/aon_task.bin" ]; then
+			echo "found aon_task bin"
+			cp tools/bin/aon_task.bin $pse_image_tools_dir/fragments
+		fi
+		if [ -f "tools/bin/bringup.bin" ]; then
+			echo "found bringup bin"
+			cp tools/bin/bringup.bin $pse_image_tools_dir/fragments
+		fi
+	else
+		pushd $bootloader_dir > /dev/null
+		cd aon_task; make SILICON;
+		cp aon_task.bin $pse_image_tools_dir/fragments
+		cd ../bringup; make SILICON
+		cp bringup.bin $pse_image_tools_dir/fragments
+		popd > /dev/null
 	fi
 	pushd $pse_image_tools_dir > /dev/null
 	python3 zephyr_image_parse.py
@@ -59,4 +84,3 @@ if [ $(test) -n "$(find build -name zephyr.elf)" ]; then
 else
 	echo "build failed"
 fi
-
