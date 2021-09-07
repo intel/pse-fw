2c23bac5afb039e3f7dc0509d319f7e48bb7b988
K Naduvalath, Sumesh <sumesh.k.naduvalath@intel.com>
2021-09-08 00:22:45 +0530

    common changes: dependency with flags added or removed
    Following changes are added:
    
    -CONFIG_MAX_THREAD_BYTES removed from base_fw/power_management/prj.conf
    
    -CONFIG_NUM_USER_APP_AND_SERVICE increased from 2 to 4 in
    		base_fw/power_management/prj.conf
    
    -added below "selects" for CONFIG_ECLITE_SERVICE
    	* CONFIG_SYS_SERVICE
    	* CONFIG_PM_SERVICE
    	* CONFIG_PMC_SERVICE
    
    -added below "selects" for CONFIG_PMC_SERVICE
    	* CONFIG_SYS_SERVICE
    
    -added below "selects" for USER_APP_FRAMEWORK
    	* CONFIG_USERSPACE
    
    -resource table entries are added based on corresponding flag
    
    - dos2unix services/sys_service/pmc_service/Kconfig
    
    Signed-off-by: K Naduvalath, Sumesh <sumesh.k.naduvalath@intel.com>

--- services/sys_service/resource_table.c
+++ services/sys_service/resource_table.c
@@ -10,15 +10,19 @@
 
 __attribute__((weak)) struct user_app_res_table_t
 	global_resource_info[CONFIG_NUM_USER_APP_AND_SERVICE] = {
-	/*PMC/System service. */
+#ifdef CONFIG_SYS_SERVICE
+	/* PMC/System service. */
 	{ .app_id = 0, .priority = SERV_BOOT_PRIO_0,
 	  .dev_list = { "IPC_HOST", "IPC_PMC"},
 	  .dev_cnt = 2 },
+#endif
+#ifdef CONFIG_ECLITE_SERVICE
 	/* ECLite service. */
 	{ .app_id = 1, .priority = SERV_BOOT_PRIO_1,
 	  .sys_service = PMC_SERVICE | HOST_SERVICE,
 	  .dev_list = { "I2C_2", "I2C_7", "IPC_HOST", "GPIO_0", "GPIO_1", "PWM_1" },
 	  .dev_cnt = 6 },
+#endif
 	/* Hello World sample App. */
 	{ .app_id = 3, .priority = APP_BOOT_PRIO_1 },
 };
