47c3f649d5337089f66187d28c24c14ad85aac29
chrislynn <chrislynn.chee@intel.com>
2022-03-16 16:28:55 +0800

    Update script install_sdk_env.sh to install_sdk_env_2.sh
    Co-authored-by: ManishJ <manish.jindgar@intel.com>

--- tools/sdk_installer/install_sdk_env_2.sh
+++ tools/sdk_installer/install_sdk_env_2.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 python_ver=3.6.9
-cmake_ver=3.13.1
+cmake_ver=3.22.1
 dtc_ver=1.4.7
 ninja_ver=1.8.2
 
@@ -39,16 +39,15 @@ done
 # Main program                                                                 #
 ################################################################################
 current_dir=$PWD
-read -p "Enter Zephyr SDK installation director(Press Enter for default path(/opt/zephyr-sdk): " installdir
+read -p "Enter Zephyr SDK installation director(Press Enter for default path(/opt/zephyr-sdk2): " installdir
 
 #Updating system and depencies for Zephyr build
 yes | sudo apt update
 yes | sudo apt upgrade
-yes | sudo apt-get install --no-install-recommends git cmake ninja-build gperf ccache doxygen graphviz dfu-util device-tree-compiler wget python3-ply python3-pip python3-setuptools python3-wheel xz-utils file make gcc gcc-multilib autoconf automake libtool
-pip3 install --user west
+yes | sudo apt-get install --no-install-recommends git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
+
 pip3 install defusedxml
 
-#Fetching and installing Zephyr SDK
 if [[ -f "$HOME/.zephyrrc" ]];
 then
 	mv ~/.zephyrrc ~/.zephyrrc.bak
@@ -59,25 +58,29 @@ else
 	chmod +x ~/.zephyrrc
 fi
 
+pip3 install --user -U west
+echo "export PATH=~/.local/bin:"$PATH"" >> ~/.zephyrrc
+
 mkdir -p ~/bin
 cd ~/bin
 
-if [[ -f "$HOME/bin/zephyr-sdk-0.10.0-setup.run" ]];
+#Fetching and installing Zephyr SDK
+if [[ -f "$HOME/bin/zephyr-sdk-0.13.1-linux-x86_64-setup.run" ]];
 then
 	echo "Zephyr SDK installer already downloaded in target directory"
-elif wget "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.10.0/zephyr-sdk-0.10.0-setup.run";
+elif wget "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.13.1/zephyr-sdk-0.13.1-linux-x86_64-setup.run";
 then
 	echo "Downloaded Zephyr SDK"
 else
 	echo -e "\e[1;31m Zephyr SDK Installer not found \e[0m"
 fi 
 
-if [[ -f "$HOME/bin/zephyr-sdk-0.10.0-setup.run" ]];
+if [[ -f "$HOME/bin/zephyr-sdk-0.13.1-linux-x86_64-setup.run" ]];
 then	
-	chmod +x zephyr-sdk-0.10.0-setup.run
+	chmod +x zephyr-sdk-0.13.1-linux-x86_64-setup.run
 	if [ -z "$installdir" ]
 	then
-		installdir=/opt/zephyr-sdk
+		installdir=/opt/zephyr-sdk2
 		echo "SDK install dir: $installdir"
 	else
 		echo "SDK install dir: $installdir"
@@ -88,11 +91,12 @@ then
 	sudo rm -rf $installdir
 	fi
 	
-	sudo ./zephyr-sdk-0.10.0-setup.run -- -d $installdir
+	sudo ./zephyr-sdk-0.13.1-linux-x86_64-setup.run -- -d $installdir
 	echo "export ZEPHYR_TOOLCHAIN_VARIANT=zephyr" >> ~/.zephyrrc
 	echo "export ZEPHYR_SDK_INSTALL_DIR=$installdir" >> ~/.zephyrrc
-	echo "export PATH=$installdir/arm-zephyr-eabi/bin/:\$PATH" >> ~/.zephyrrc
+#	echo "export PATH=$installdir/arm-zephyr-eabi/bin/:\$PATH" >> ~/.zephyrrc
 fi
+
 
 #Checking Python version 
 var="$(python3 --version 2>&1)"
@@ -120,26 +124,27 @@ then
 else
 	echo "Installing Cmake $cmake_ver"
 	mkdir -p $HOME/bin/cmake && cd $HOME/bin/cmake
-	if [[ -f "$HOME/bin/cmake/cmake-3.13.1.tar.gz" ]];
+	if [[ -f "$HOME/bin/cmake/cmake-3.22.1.tar.gz" ]];
 	then
 		echo "Cmake installer already downloaded in target directory"
-	elif wget "https://github.com/Kitware/CMake/releases/download/v3.13.1/cmake-3.13.1.tar.gz"; 
+	elif wget "https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz"; 
 	then
 		echo "Downloaded Cmake installer"
     else 
 		echo -e "\e[1;31m Cmake $cmake_ver Installer not found \e[0m"
 	fi
 	
-	if [[ -f "$HOME/bin/cmake/cmake-3.13.1.tar.gz" ]];
+	if [[ -f "$HOME/bin/cmake/cmake-3.22.1.tar.gz" ]];
 	then
-		sudo rm -rf cmake-3.13.1
-		sudo tar -zxvf cmake-3.13.1.tar.gz
-		cd cmake-3.13.1
+		sudo rm -rf cmake-3.22.1
+		sudo tar -zxvf cmake-3.22.1.tar.gz
+		cd cmake-3.22.1
 		yes | sudo apt-get install build-essential
+		yes | sudo apt-get install libssl-dev
 		sudo ./bootstrap
 		sudo make
 		sudo make install
-		echo "export PATH=\$PATH:$PWD/bin" >> $HOME/.zephyrrc
+		#echo "export PATH=\$PATH:$PWD/bin" >> $HOME/.zephyrrc
 	fi
 fi
 
@@ -174,36 +179,9 @@ else
     fi
 fi
 
-#Checking Ninja version 
-var="$(ninja --version 2>&1)"
-if [[ $var =~ "$ninja_ver" ]] 
-then
-	echo "ninja $ninja_ver already installed"
-else
-	echo "Installing ninja $ninja_ver"
-	cd ~/bin
-	
-	if [[ -f "$HOME/bin/ninja-linux.zip" ]];
-	then
-		echo "Ninja installer already downloaded in target directory"
-	elif wget "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip";
-	then 
-		echo "Downloaded Ninja installer"
-    else 
-    echo -e "\e[1;31m ninja $ninja_ver Installer not found \e[0m"
-	fi
-	
-	if [[ -f "$HOME/bin/ninja-linux.zip" ]];
-	then
-		sudo rm -rf ninja
-		unzip ninja-linux.zip
-		echo "export PATH=$PWD/ninja:\$PATH" >> ~/.zephyrrc
-	fi
-fi
-
 #Installing additional Python packages required by Zephyr
 cd $current_dir
-cd ../../modules/rtos/pse_zephyr
+cd ../../../zephyr
 pip3 install --user -r scripts/requirements.txt
 
 source ~/.zephyrrc
@@ -247,22 +225,11 @@ else
 	echo -e "\e[1;31m Install device-tree-compiler (dtc) version $dtc_ver or higher manually \e[0m"
 fi
 
-#Checking Ninja version
-ver_nja=0 
-var="$(ninja --version 2>&1)"
-if [[ $var =~ "$ninja_ver" ]] 
-then
-	ver_nja=1
-else
-	echo -e "\e[1;31m Install ninja version $ninja_ver manually \e[0m"
-fi
-
-if [[ $ver_py -eq 1 && $ver_cmk -eq 1 && $ver_dtc -eq 1 && $ver_nja -eq 1 ]]
+if [[ $ver_py -eq 1 && $ver_cmk -eq 1 && $ver_dtc -eq 1 ]]
 then
 	python3 --version
 	cmake --version
 	dtc --version
-	ninja --version
 	echo -e "\e[1;32m \n Finished Installation \e[0m"
 else
 	echo -e "\e[1;31m \n Check above errors \e[0m"
